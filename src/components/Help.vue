2563aa571d43b2bb20dae1f52eab7bec97a5fba0
Darlan Alves <me@darlanalv.es>
2023-05-23 14:51:13 +0000

    chore: compact layout for commands

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -3,14 +3,11 @@
     <h1 class="text-2xl font-bold mb-4">Available Commands</h1>
     <div v-for="(subcommands, parent) in commands"
       :key="parent"
-      class="mb-6">
-      <h2 class="text-2xl font-semibold mb-2">{{ parent }}</h2>
-      <ul class="list-disc pl-5">
+      class="mb-4">
+      <h2 class="text-lg font-semibold mb-2">{{ parent }}</h2>
+      <ul class="pl-4">
         <li v-for="command in subcommands"
-          :key="command"
-          class="mb-1 p-2">
-          {{ command }}
-        </li>
+          :key="command">{{ command }}</li>
       </ul>
     </div>
   </div>
