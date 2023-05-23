7b6956f0980164489feaba5fd0adabe58ff7f220
Darlan Alves <me@darlanalv.es>
2023-05-23 13:54:19 +0000

    fix: use hash for navigation

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,16 +3,16 @@ import Settings from './components/Settings.vue';
 
 export default [
   {
-    path: '/help',
-    name: 'Help',
-    icon: 'help_outline',
-    component: Help
-  },
-  {
     path: '/settings',
     name: 'Settings',
     icon: 'settings',
     component: Settings
+  },
+  {
+    path: '/help',
+    name: 'Help',
+    icon: 'help_outline',
+    component: Help
   },
 ];
 
