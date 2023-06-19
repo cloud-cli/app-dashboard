8a722ff7d0d66eb11598c12fc7e52ff687dccbf1
Darlan Alves <me@darlanalv.es>
2023-06-19 15:48:12 +0200

    chore: duh

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template >
-  <template>
-    <!-- Sticky header -->
-    <div class="z-50 bg-white flex justify-between items-center px-4 py-2 shadow-md">
-      <h1 class="text-2xl font-bold">Cloudy</h1>
-      <div>Hi, {{ profile.displayName }}</div>
+  <!-- Sticky header -->
+  <div class="z-50 bg-white flex justify-between items-center px-4 py-2 shadow-md">
+    <h1 class="text-2xl font-bold">Cloudy</h1>
+    <div>Hi, {{ profile.displayName }}</div>
+  </div>
+
+  <!-- Sidebar -->
+  <div class="h-screen flex overflow-hidden bg-white">
+    <div class="flex flex-col w-64 border-r border-gray-200 pb-4 bg-white">
+      <nav class="mt-5 flex-1 space-y-1">
+        <router-link v-for="route in routes"
+          :to="route.path"
+          class="flex items-center text-gray-900 space-x-2 py-2 px-4"
+          active-class="text-blue-500 bg-gray-200">
+          <span class="material-icons text-base">{{ route.icon }}</span>
+          <span class="text-base">{{ route.name }}</span>
+        </router-link>
+      </nav>
     </div>
 
-    <!-- Sidebar -->
-    <div class="h-screen flex overflow-hidden bg-white">
-      <div class="flex flex-col w-64 border-r border-gray-200 pb-4 bg-white">
-        <nav class="mt-5 flex-1 space-y-1">
-          <router-link v-for="route in routes"
-            :to="route.path"
-            class="flex items-center text-gray-900 space-x-2 py-2 px-4"
-            active-class="text-blue-500 bg-gray-200">
-            <span class="material-icons text-base">{{ route.icon }}</span>
-            <span class="text-base">{{ route.name }}</span>
-          </router-link>
-        </nav>
-      </div>
-
-      <!-- Main area -->
-      <div class="flex-1 overflow-auto pt-4">
-        <router-view></router-view>
-        <Error v-if="error" :error="error.message" />
-      </div>
+    <!-- Main area -->
+    <div class="flex-1 overflow-auto pt-4">
+      <router-view></router-view>
+      <Error v-if="error" :error="error.message" />
     </div>
-  </template>
+  </div>
 </template>
 
 <script setup>
