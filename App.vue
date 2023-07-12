7d937e77cb18bd963119737d30d744749f1bd7f0
Darlan Alves <me@darlanalv.es>
2023-07-12 19:31:09 +0200

    chore: move login/logout buttons

--- App.vue
+++ App.vue
@@ -1,41 +1,39 @@
 <template>
-  <div class="relative">
-    <div class="absolute z-50 right-0 top-0">
-      <button
-        v-if="authHost && isLoggedIn"
-        class="bg-gray-300 text-sm px-4 py-2"
-        @click="logout()"
-      >
-        Logout
-      </button>
-      <button
-        v-if="authHost && !isLoggedIn"
-        class="bg-blue-500 text-sm px-4 py-2"
-        @click="goToLogin()"
-      >
-        Sign in
-      </button>
+  <div class="h-screen flex overflow-hidden bg-white">
+    <div class="flex flex-col md:w-64 border-r border-gray-200 pb-4 bg-white">
+      <nav class="mt-5 flex-1 space-y-1">
+        <router-link
+          v-for="route in enabledRoutes"
+          :to="route.path"
+          class="flex items-center text-gray-900 space-x-2 py-2 px-4"
+          active-class="text-blue-500 bg-gray-200"
+        >
+          <span class="material-icons text-base">{{ route.icon }}</span>
+          <span class="text-base hidden md:block">{{ route.name }}</span>
+        </router-link>
+      </nav>
+      <hr class="mb-4" />
+      <div class="text-center">
+        <button
+          v-if="authHost && isLoggedIn"
+          class="bg-gray-300 text-sm px-4 py-2 shadow-md"
+          @click="logout()"
+        >
+          Logout
+        </button>
+        <button
+          v-if="authHost && !isLoggedIn"
+          class="bg-blue-500 text-sm px-4 py-2 shadow-md"
+          @click="goToLogin()"
+        >
+          Sign in
+        </button>
+      </div>
     </div>
 
-    <div class="h-screen flex overflow-hidden bg-white">
-      <div class="flex flex-col md:w-64 border-r border-gray-200 pb-4 bg-white">
-        <nav class="mt-5 flex-1 space-y-1">
-          <router-link
-            v-for="route in enabledRoutes"
-            :to="route.path"
-            class="flex items-center text-gray-900 space-x-2 py-2 px-4"
-            active-class="text-blue-500 bg-gray-200"
-          >
-            <span class="material-icons text-base">{{ route.icon }}</span>
-            <span class="text-base hidden md:block">{{ route.name }}</span>
-          </router-link>
-        </nav>
-      </div>
-
-      <div class="flex-1 overflow-auto pt-4">
-        <router-view></router-view>
-        <Error v-if="error" :error="error.message" />
-      </div>
+    <div class="flex-1 overflow-auto pt-4">
+      <router-view></router-view>
+      <Error v-if="error" :error="error.message" />
     </div>
   </div>
 </template>
