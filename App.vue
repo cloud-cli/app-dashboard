1398eaff6b92004509a1ad6b148e5a86fdb35399
Darlan Alves <me@darlanalv.es>
2023-07-19 22:52:12 +0200

    fix: update composables in App.vue

--- App.vue
+++ App.vue
@@ -1,42 +1,39 @@
 <template>
   <div class="h-screen flex overflow-hidden bg-white">
-    <Spinner class="my-6 mx-auto" v-if="loading" />
-    <template v-else>
-      <div class="flex flex-col md:w-64 border-r border-gray-200 pb-4 bg-white">
-        <nav class="pt-2 flex-1 space-y-1">
-          <router-link
-            v-for="route in enabledRoutes"
-            :to="route.path"
-            class="flex items-center text-gray-900 space-x-2 py-2 px-4"
-            active-class="text-blue-500 bg-gray-200"
-          >
-            <span class="material-icons">{{ route.icon }}</span>
-            <span class="text-base hidden md:block">{{ route.name }}</span>
-          </router-link>
-        </nav>
-        <button
-          v-if="authHost && isLoggedIn"
-          class="bg-gray-100 text-sm px-4 py-2 shadow-md flex items-center mx-auto"
-          @click="logout()"
+    <div class="flex flex-col md:w-64 border-r border-gray-200 pb-4 bg-white">
+      <nav class="pt-2 flex-1 space-y-1">
+        <router-link
+          v-for="route in enabledRoutes"
+          :to="route.path"
+          class="flex items-center text-gray-900 space-x-2 py-2 px-4"
+          active-class="text-blue-500 bg-gray-200"
         >
-          <span class="material-icons">logout</span>
-          <span class="hidden md:inline">Logout</span>
-        </button>
-        <button
-          v-if="authHost && !isLoggedIn"
-          class="bg-blue-500 text-white text-sm px-4 py-2 shadow-md flex items-center mx-auto"
-          @click="goToLogin()"
-        >
-          <span class="material-icons">person</span>
-          <span class="hidden md:inline">Sign in</span>
-        </button>
-      </div>
+          <span class="material-icons">{{ route.icon }}</span>
+          <span class="text-base hidden md:block">{{ route.name }}</span>
+        </router-link>
+      </nav>
+      <button
+        v-if="authHost && isLoggedIn"
+        class="bg-gray-100 text-sm px-4 py-2 shadow-md flex items-center mx-auto"
+        @click="logout()"
+      >
+        <span class="material-icons">logout</span>
+        <span class="hidden md:inline">Logout</span>
+      </button>
+      <button
+        v-if="authHost && !isLoggedIn"
+        class="bg-blue-500 text-white text-sm px-4 py-2 shadow-md flex items-center mx-auto"
+        @click="goToLogin()"
+      >
+        <span class="material-icons">person</span>
+        <span class="hidden md:inline">Sign in</span>
+      </button>
+    </div>
 
-      <div class="flex-1 overflow-y-auto shadow-lg">
-        <Error v-if="error" :error="error.message" />
-        <router-view></router-view>
-      </div>
-    </template>
+    <div class="flex-1 overflow-y-auto shadow-lg">
+      <Error v-if="error" :error="error.message" />
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -49,10 +46,10 @@ import { useCommands } from "./composables/useCommands";
 import { useEnv } from "./composables/useEnv";
 import { useRouter } from "./composables/useRouter";
 
-const { isLoggedIn, logout, goToLogin, ready } = useAuth();
-const { error, fetchCommands } = useCommands();
+const { authHost } = await useEnv();
+const { isLoggedIn, logout, goToLogin } = await useAuth();
+const { error, fetchCommands } = await useCommands();
 const { topPages } = useRouter();
-const { authHost } = useEnv();
 
 const enabledRoutes = computed(() => {
   const auth = isLoggedIn.value;
@@ -64,13 +61,5 @@ const enabledRoutes = computed(() => {
   });
 });
 
-const loading = ref(true);
-
-ready.then(() => {
-  if (authHost.value) {
-    fetchCommands();
-  }
-
-  loading.value = false;
-});
+fetchCommands();
 </script>
